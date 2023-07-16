5bfeda008db577769dbe49f562c783f769439a57
Shiyam Robert <122890678+shiyammm@users.noreply.github.com>
2023-07-16 19:26:44 +0530

    Add files via upload

---
+++ tailwindcss-config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        reaction: "hsl(0, 100%, 67%)",
+        bgReaction: "hsl(0, 100%, 67%,.1)",
+        memory: "hsl(39, 100%, 56%)",
+        bgMemory: "hsl(39, 100%, 56%,.1)",
+        verbal: "hsl(166, 100%, 37%)",
+        bgVerbal: "hsl(166, 100%, 37%,.1)",
+        visual: " hsl(234, 85%, 45%)",
+        bgVisual: " hsl(234, 85%, 45%,.1)",
+        slateBlue: "hsl(252, 100%, 67%)",
+        royalBlue: "hsl(241, 81%, 54%)",
+        violetBlue: "hsla(256, 72%, 46%, 1)",
+        persianBlue: "hsla(241, 72%, 46%, 0)",
+        white: "hsl(0, 0%, 100%)",
+        paleBlue: "hsl(221, 100%, 96%)",
+        lightLavender: "hsl(241, 100%, 89%)",
+        darkGrayBlue: "hsl(224, 30%, 27%)",
+      },
+      fontFamily: {
+        han: ["Hanken Grotesk", "sans-serif"],
+      },
+    },
+  },
+};
